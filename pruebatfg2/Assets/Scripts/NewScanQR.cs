2e3c35b2453599a4ab92abc504861b2ffc554b64
Kralosxd <potatuncio@gmail.com>
2025-06-08 20:34:56 +0200

    boton escaneo

---
+++ pruebatfg2/Assets/Scripts/NewScanQR.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Google.XR.Cardboard;
+
+public class NewScanQR : MonoBehaviour
+{
+    /// <summary>
+    /// The material to use when this object is inactive (not being gazed at).
+    /// </summary>
+    public Material InactiveMaterial;
+
+    /// <summary>
+    /// The material to use when this object is active (gazed at).
+    /// </summary>
+    public Material GazedAtMaterial;
+
+    /// <summary>
+    /// The material to use when this object is active (gazed at).
+    /// </summary>
+    public Material ClickedMaterial;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    /// <summary>
+    /// This method is called by the Main Camera when it starts gazing at this GameObject.
+    /// </summary>
+    public void OnPointerEnter()
+    {
+        GetComponent<Renderer>().material = GazedAtMaterial;
+    }
+
+    /// <summary>
+    /// This method is called by the Main Camera when it stops gazing at this GameObject.
+    /// </summary>
+    public void OnPointerExit()
+    {
+        GetComponent<Renderer>().material = InactiveMaterial;
+    }
+
+    /// <summary>
+    /// This method is called by the Main Camera when it is gazing at this GameObject and the screen
+    /// is touched.
+    /// </summary>
+    public void OnPointerClick()
+    {
+        GetComponent<Renderer>().material = ClickedMaterial;
+
+        //Api.SaveDeviceParams(uri);
+        //Api.UpdateScreenParams();
+
+    }
+}
